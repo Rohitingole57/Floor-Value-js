c763ecaedf4026444ca7b913bcb9427d64bc9605
Rohit Ingole <ingolerohit2017@gmail.com>
2023-02-14 12:06:42 +0530

    Finding the floor avlues of given numbers

---
+++ index.js
@@ -0,0 +1,11 @@
+ // find the floor value of (32/N) if n == 0; print -1 and if x ==0; print TOO LOW
+
+ var n=5;
+ var x= 32/n;
+ if(n==0){
+    console.log("-1");
+ }else if(x==0) {
+    console.log("TOO LOW");
+ }else {
+    console.log(x)
+ }
